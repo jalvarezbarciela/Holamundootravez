dad1ccdcf9e26c7b178ab68ec344d3f6345e2341
jalvarezbarciela <jalvarezbarciela@danielcastelao.org>
2019-11-08 14:19:51 +0100

    Segundo commit

--- src/com/company/HolaMundo.java
+++ src/com/company/HolaMundo.java
@@ -3,6 +3,6 @@ package com.company;
 public class HolaMundo {
 
     public static void main(String[] args) {
-	System.out.println("Hola mundo");
+	System.out.println("Hola mundo 2 el retorno ahora en 4k");
 	}
 }
